16bcb0c41cb2901d17d6110438a781340fc79287
kvanme01 <kristi.vanmeter@duke.edu>
2022-10-04 14:27:20 -0400

    add data

---
+++ Class-929.py
@@ -0,0 +1,10 @@
+# file for class 9/29
+
+import pandas as pd
+
+small = pd.read_csv("wdi_small_tidy_2015.csv")
+
+small.columns
+
+subet_small = small[["Mortality rate, infant (per 1,000 live births)", "GDP per capita (constant 2010 US$)", "Country Name"]]
+
